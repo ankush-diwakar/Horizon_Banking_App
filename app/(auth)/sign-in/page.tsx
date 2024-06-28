0eb438a49a5c5fe9dcaf2068514d137eed08aa6b
ankush-diwakar <150348342+ankush-diwakar@users.noreply.github.com>
2024-06-28 14:13:55 +0530

    Created project and did assets setup

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SignIn = () => {
+  return (
+    <div>
+       <p>i am sign In</p> 
+    </div>
+  )
+}
+
+export default SignIn
---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SignUp = () => {
+  return (
+    <div>
+      <p>sign Up</p>
+    </div>
+  )
+}
+
+export default SignUp
