7c0d88b5381e0537b13b1a07e8cfafaa3e041696
ankush-diwakar <150348342+ankush-diwakar@users.noreply.github.com>
2024-06-29 21:38:31 +0530

    make the SignUp and SignIn Form functional with all validations and schema of the forms using zod and shadcn form

--- lib/utils.ts
+++ lib/utils.ts
@@ -195,17 +195,34 @@ export const getTransactionStatus = (date: Date) => {
   return date > twoDaysAgo ? "Processing" : "Success";
 };
 
-export const authFormSchema = (type: string) => z.object({
-  // sign up
-  firstName: type === 'sign-in' ? z.string().optional() : z.string().min(3),
-  lastName: type === 'sign-in' ? z.string().optional() : z.string().min(3),
-  address1: type === 'sign-in' ? z.string().optional() : z.string().max(50),
-  city: type === 'sign-in' ? z.string().optional() : z.string().max(50),
-  state: type === 'sign-in' ? z.string().optional() : z.string().min(2).max(2),
-  postalCode: type === 'sign-in' ? z.string().optional() : z.string().min(3).max(6),
-  dateOfBirth: type === 'sign-in' ? z.string().optional() : z.string().min(3),
-  ssn: type === 'sign-in' ? z.string().optional() : z.string().min(3),
-  // both
+// export const authFormSchema = (type: string) => z.object({
+//   // sign up
+//   firstName: type === 'sign-in' ? z.string().optional() : z.string().min(3),
+//   lastName: type === 'sign-in' ? z.string().optional() : z.string().min(3),
+//   address1: type === 'sign-in' ? z.string().optional() : z.string().max(50),
+//   city: type === 'sign-in' ? z.string().optional() : z.string().max(50),
+//   state: type === 'sign-in' ? z.string().optional() : z.string().min(2).max(2),
+//   postalCode: type === 'sign-in' ? z.string().optional() : z.string().min(3).max(6),
+//   dateOfBirth: type === 'sign-in' ? z.string().optional() : z.string().min(3),
+//   ssn: type === 'sign-in' ? z.string().optional() : z.string().min(3),
+//   // both
+//   email: z.string().email(),
+//   password: z.string().min(8),
+// })
+
+
+export const authFormSchema = (type:string) => z.object({
+  //for sign-up
+  firstName : type === 'sign-in' ? z.string().optional() : z.string().min(3),
+  lastName : type === 'sign-in' ? z.string().optional() : z.string().min(3),
+  address1 : type === 'sign-in' ? z.string().optional() : z.string().max(50), 
+  city : type === 'sign-in' ? z.string().optional() : z.string().max(10), 
+  state : type === 'sign-in' ? z.string().optional() : z.string().min(2).max(2),
+  postalCode : type === 'sign-in' ? z.string().optional() : z.string().min(3),
+  dateOfBirth : type === 'sign-in' ? z.string().optional() : z.string().min(3),
+  ssn : type === 'sign-in' ? z.string().optional() : z.string().min(3),
+
+  //for sign-in and sign-up both
   email: z.string().email(),
-  password: z.string().min(8),
-})
+  password : z.string().min(8),
+});
